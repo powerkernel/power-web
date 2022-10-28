9c024a988bb9fca105cef6d48a7de36c9eab47bf
harrytang <harry@powerkernel.com>
2022-10-28 15:07:33 +0300

    feat: built home page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'powerkernel.github.io',
+        // port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Power Kernel</title>
         <meta
@@ -12,7 +13,34 @@ export default function Home() {
         <meta name="keywords" content="powerkernel, power, kernel, web development, design, consultation"></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-3xl font-bold underline text-red-400">Hello world!</h1>
-    </div>
+
+      <div className="h-screen container mx-auto p-4">
+        <div className="h-5/6 grid grid-cols-1 content-center">
+          <div className="flex justify-center">
+            <Image
+              className=""
+              width={300}
+              height={127}
+              src="https://powerkernel.github.io/powerkernel.com/logos/logo-rectangle.svg"
+              alt="Power Kernel"
+            />
+          </div>
+
+          <div className="flex justify-center py-5">
+            Providing IT solutions such as customized system development, web design & development, and IT related
+            consultation..
+          </div>
+
+          <div className="flex justify-center">
+            <a
+              href="mailto:harry@powerkernel.com"
+              className="flex items-center justify-center rounded-md border border-transparent bg-blue-500 px-8 py-3 text-base font-medium text-white hover:bg-blue-600 md:py-4 md:px-10 md:text-lg"
+            >
+              Contact Us
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }

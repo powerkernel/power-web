ea00172a0000beea509c6e9b19eab8f1ee30c855
harrytang <harry@powerkernel.com>
2022-11-18 16:35:32 +0200

    chore: use node 16

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,7 +1,7 @@
 describe("Navigation", () => {
   it("should navigate to the about page", () => {
     // Start from the index page
-    cy.visit("http://localhost:3000/");
+    cy.visit("/");
 
     // The new page should contain an p tag with "About page"
     cy.get("p").contains("Providing IT solutions");

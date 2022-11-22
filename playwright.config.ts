47e96c61c7e0f47415d82dd5b85c9f8515f02468
harrytang <harry@powerkernel.com>
2022-11-23 00:00:50 +0200

    chore: config e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,12 +2,12 @@ import type { PlaywrightTestConfig } from "@playwright/test";
 const config: PlaywrightTestConfig = {
   webServer: {
     command: "npm run start",
-    url: "http://localhost:3000",
+    url: "http://127.0.0.1:3000",
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
   use: {
-    baseURL: "http://localhost:3000",
+    baseURL: "http://127.0.0.1:3000",
   },
 };
 export default config;

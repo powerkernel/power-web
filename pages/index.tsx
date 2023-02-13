c93f89e1d79db5699cd0fd834aa06ecde47b8d63
harrytang <harry@powerkernel.com>
2023-02-13 11:45:09 +0000

    chore: adds staging and misc configs

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,7 @@ export default function Home() {
           </div>
 
           <div className="flex justify-center py-5 text-xs text-gray-300">
-            Version: 0.1.1-build.2301162051
+            Version: 0.1.4-build.2302101105
           </div>
         </div>
       </div>
